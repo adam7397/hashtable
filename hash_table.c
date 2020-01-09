5f5f6663803127f5dc20a36ea98a184ceb3f179e
Adam7397 <adamsbradfield@gmail.com>
2020-01-08 21:09:06 -0700

    allocation & put is working

---
+++ hash_table.c
@@ -0,0 +1,136 @@
+#include "hash_table.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+// Initialize the components of a hashtable.
+// The size parameter is the expected number of elements to be inserted.
+// This method returns an error code, 0 for success and -1 otherwise (e.g., if the parameter passed to the method is not null, if malloc fails, etc).
+int allocate(hashtable** ht, int size) {
+
+  // Check the size of the paramter passed in, if it is too small initialize nothing
+  if (size < 1 ) {
+    return -1;
+  }
+  
+  if(((*ht) = malloc(sizeof(hashtable))) == NULL){
+    return -1;
+  }
+
+  // Once we have confirmed the size, we need to create the array by allocating memory
+  // Pass the pointer of the array back so that the program can access it or something
+  if(((*ht)->entries = malloc(sizeof(node*) * size)) == NULL){
+    return -1;
+  }
+  (*ht)->size = size;
+  (*ht)->inserted = 0;
+  (*ht)->tuning = 0;
+  
+  // Set the entires to point to null
+  for(int i = 0; i < size; ++i){
+    (*ht)->entries[i] = NULL;
+  }
+  return 0;
+}
+
+// This method inserts a key-value pair into the hash table.
+// It returns an error code, 0 for success and -1 otherwise (e.g., if malloc is called and fails).
+int put(hashtable* ht, keyType key, valType value) {
+    // Use our hash function here to determine which index the value should be placed
+    int slot = key % ht->size;
+
+    // Need to allocate memory for this node before declaring it STUPID ASS PYTHON ASS USER
+    // Create a new node for the value
+    struct node *newNode = malloc(sizeof(struct node));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = NULL;
+
+    // Checking here for if there is anything in the index yet
+    if (ht->entries[slot] == NULL){
+      ht->entries[slot] = newNode;
+    }
+    // If there is, we will need to traverse the linked list to update the teminal node next pointer
+    else {
+      node *endNode = ht->entries[slot];
+      while(endNode->next != NULL){
+        endNode = endNode->next;
+      }
+
+      endNode->next = newNode;
+    }
+
+    return 0;
+}
+
+// This method retrieves entries with a matching key and stores the corresponding values in the
+// values array. The size of the values array is given by the parameter
+// num_values. If there are more matching entries than num_values, they are not
+// stored in the values array to avoid a buffer overflow. The function returns
+// the number of matching entries using the num_results pointer. If the value of num_results is greater than
+// num_values, the caller can invoke this function again (with a larger buffer)
+// to get values that it missed during the first call. 
+// This method returns an error code, 0 for success and -1 otherwise (e.g., if the hashtable is not allocated).
+int get(hashtable* ht, keyType key, valType *values, int num_values, int* num_results) {
+    (void) ht;
+    (void) key;
+    (void) values;
+    (void) num_values;
+    (void) num_results;
+
+    int slot = key % sizeof(ht);
+
+    struct node *entry = ht->entries[slot];
+
+    if(entry == NULL){
+      printf("There are no matching hashed keys");
+      return -1;
+    }
+
+    while(entry != NULL){
+      if(entry->key == key){
+        printf("Found it here");
+        
+      }
+    }
+
+
+    return 0;
+}
+
+// This method erases all key-value pairs with a given key from the hash table.
+// It returns an error code, 0 for success and -1 otherwise (e.g., if the hashtable is not allocated).
+int erase(hashtable* ht, keyType key) {
+    (void) ht;
+    (void) key;
+
+    int slot = key % sizeof(ht);
+
+    if (ht->entries [slot] == NULL) {
+      return -1;
+    }
+    else {
+      struct node *firstNode = ht->entries[slot];
+      struct node *secondNode = firstNode->next;
+      while(secondNode->next != NULL){
+        if (secondNode->key == key) {
+          firstNode->next = secondNode->next;
+        }
+        // Deallocate the secondNode here somehow
+        free(secondNode);
+      }
+    }
+    return 0;
+}
+
+// This method frees all memory occupied by the hash table.
+// It returns an error code, 0 for success and -1 otherwise.
+int deallocate(hashtable* ht) {
+    // This line tells the compiler that we know we haven't used the variable
+    // yet so don't issue a warning. You should remove this line once you use
+    // the parameter.
+    (void) ht;
+    free(ht);
+    return 0;
+}
---
+++ hash_table.h
@@ -0,0 +1,27 @@
+#ifndef CS165_HASH_TABLE // This is a header guard. It prevents the header from being included more than once.
+#define CS165_HASH_TABLE  
+
+typedef struct {
+// define the components of the hash table here (e.g. the array, bookkeeping for number of elements, etc)
+  int size;
+  int inserted;
+  int tuning;
+  struct node **entries;
+} hashtable;
+
+typedef struct node {
+  int key;
+  int value;
+  struct node *next;
+} node;
+
+typedef int keyType;
+typedef int valType;
+
+int allocate(hashtable** ht, int size);
+int put(hashtable* ht, keyType key, valType value);
+int get(hashtable* ht, keyType key, valType *values, int num_values, int* num_results);
+int erase(hashtable* ht, keyType key);
+int deallocate(hashtable* ht);
+
+#endif
